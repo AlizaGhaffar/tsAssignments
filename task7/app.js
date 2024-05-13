e9651166cfe566c201c98c9698a610388c4bdf6f
AlizaGhaffar <164041342+AlizaGhaffar@users.noreply.github.com>
2024-05-13 04:58:47 -0700

    completed task7

---
+++ task7/app.js
@@ -0,0 +1,21 @@
+// ADDITION
+var num1 = 6;
+var num2 = 2;
+var addition = num1 + num2; //8
+console.log(addition);
+;
+//SUBTRACTION
+var number1 = 12;
+var number2 = 4;
+var subtraction = number1 - number2; //8
+console.log(subtraction);
+//MULTIPLICATION
+var value1 = 4;
+var value2 = 2;
+var multiplication = value1 * value2; //8
+console.log(multiplication);
+//DIVISION
+var digit1 = 16;
+var digit2 = 2;
+var division = digit1 / digit2; //8
+console.log(division);
